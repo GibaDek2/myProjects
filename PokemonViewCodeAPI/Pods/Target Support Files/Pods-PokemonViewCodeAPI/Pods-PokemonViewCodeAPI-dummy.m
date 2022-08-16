6eb8e050df1cb99d7f58af7c2c2bd186fa1fba38
Gilberto Amaro <gibovsky1@gmail.com>
2022-08-16 11:11:24 -0300

    Alamofire,SnapKit,SDwebImage adc

---
+++ PokemonViewCodeAPI/Pods/Target Support Files/Pods-PokemonViewCodeAPI/Pods-PokemonViewCodeAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokemonViewCodeAPI : NSObject
+@end
+@implementation PodsDummy_Pods_PokemonViewCodeAPI
+@end
---
+++ PokemonViewCodeAPI/Pods/Target Support Files/Pods-PokemonViewCodeAPITests/Pods-PokemonViewCodeAPITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokemonViewCodeAPITests : NSObject
+@end
+@implementation PodsDummy_Pods_PokemonViewCodeAPITests
+@end
