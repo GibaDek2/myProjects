47736d7d73989fbd6cfc9ce4133c7842fafe4991
Gilberto Amaro <gibovsky1@gmail.com>
2022-08-16 11:43:30 -0300

    PokemonListController criada

---
+++ PokemonViewCodeAPI/PokemonViewCodeAPI/Controller/PokemonListController.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonListController.swift
+//  PokemonViewCodeAPI
+//
+//  Created by Gilberto Amaro on 16/08/22.
+//
+
+import UIKit
+
+class PokemonListController: NSObject {
+    let showAlert = "Alert!"
+    let showMessage = "Request Fail"
+    let showButton = "OK"
+    let titleOnTop = "My Pokemon List"
+    let myRequest = RequestPokemon()
+    var myPokemon: PokemonList?
+    
+    var heightForRowAr: CGFloat {
+        return 70
+    }
+    
+    var numberOfRowsInTableView: Int {
+        return myPokemon?.results?.count ?? 0
+    }
+    
+    func getResultByIndexPath(indexPath: IndexPath) -> Result? {
+        return myPokemon?.results?[indexPath.row]
+    }
+    
+    func requestListController(completion: @escaping(Bool) -> Void) {
+        myRequest.requestList { [weak self] myList, success in
+            if success {
+                self?.myPokemon = myList
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+}
